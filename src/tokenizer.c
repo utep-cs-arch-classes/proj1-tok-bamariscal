229759c8cbbe4f702f8b17ae175bc0f89145405a
bamariscal <bamariscal@miners.utep.edu>
2021-02-19 12:23:14 -0700

    built skeleton of all 5 methods

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,4 +1,31 @@
+#include "tokenizer.h"
 
-void main(){
 
+int space_char(char c){//true if c is a tab or space, and not zero
+
+  
+}
+
+
+int non_space_char(char c){//true if c not tab or space, and not zero
+
+  
+}
+
+
+char *word_start(char *str){//* to first non-space char in first word in s
+
+  
+}
+
+
+char *word_end(char *str){//* for space cahr after end of word
+
+  
+}
+
+
+int count_words(char *str){//the number of words in s
+
+  
 }
