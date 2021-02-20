43a77d4281763acb6d91a6617842c0d55b087b90
bamariscal <bamariscal@miners.utep.edu>
2021-02-20 11:58:02 -0700

    Worked on the body of the first 2  methods

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -3,18 +3,24 @@
 
 int space_char(char c){//true if c is a tab or space, and not zero
 
+  if(c ==' ' || c == '\t' || c == '\n'){
+    return true;
+  }
+  return false
   
 }
 
 
 int non_space_char(char c){//true if c not tab or space, and not zero
-
+  if(c !=' ' || c != 't' || c != '\n'){
+    return true;
+  }
+  return false;
   
 }
 
 
 char *word_start(char *str){//* to first non-space char in first word in s
-
   
 }
 
