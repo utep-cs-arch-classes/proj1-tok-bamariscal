08202f54688a8bd2496aacacca2ea3a0bd61917d
bamariscal <bamariscal@miners.utep.edu>
2021-02-21 14:07:47 -0700

    added body of code on the last 3 methods

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -2,36 +2,53 @@
 
 
 int space_char(char c){//true if c is a tab or space, and not zero
-
   if(c ==' ' || c == '\t' || c == '\n'){
-    return true;
+    return 1;
   }
-  return false
+  return 0;
   
 }
 
 
 int non_space_char(char c){//true if c not tab or space, and not zero
   if(c !=' ' || c != 't' || c != '\n'){
-    return true;
+    return 1;
   }
-  return false;
+  return 0;
   
 }
 
 
 char *word_start(char *str){//* to first non-space char in first word in s
-  
+  while(non_space_char(*str) || space_char(*str)){
+      str++;
+    }
+  return str;
 }
 
 
-char *word_end(char *str){//* for space cahr after end of word
+char *word_end(char *str){//* for space char after end of word
+  if(space_char(*str)){
+    str = word_start(str);
+  }
 
-  
+  while(non_space_char(*str)){
+    str++;
+  }
+  return str;
 }
 
 
 int count_words(char *str){//the number of words in s
+  int count =0;
 
-  
+  while (*str != '\0'){
+    if(space_char(*str))
+      count++;
+    
+    str++;
+  }
+  return count;
 }
+  
+
