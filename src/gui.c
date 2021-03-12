a953510b2cc5adb739302365b091b2f7bf185aea
bamariscal <bamariscal@miners.utep.edu>
2021-03-12 06:57:27 -0700

    added implementation for history

--- src/gui.c
+++ src/gui.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "tokenizer.h"
+#include "history.h"
 
 #define LIMIT 100
 
 void main(){
-  char input;
-  char arr[LIMIT];
 
-  printf("enter any value\n");
+  char i;
+  char input;
+
+  List *history = init_history();
+  Item * temp;
+  while(1){
+    char arr[LIMIT];
+    i =0;
+  printf("Enter an input\n");
 
   for(input; (input = getchar()) != '\n'; arr[LIMIT]++){
     arr[LIMIT] = input;
     putchar(input);
   }
 
-  printf("\n");
+   printf("\n");
+
+  arr[i]= '\0';
+
+  add_history(history,arr);
+  printf("\n There is %d words ", count_words(arr));
+  char**tokens = tokenize(arr);
+  print_tokens(tokens);
+  }
+  print_history(history);
 }
